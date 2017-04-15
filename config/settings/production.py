fe8d1f26984b5761bebe045e17b180a32f50352b
taedori <wlminimal@gmail.com>
2017-04-14 18:03:26 -0700

    Fix allowed_host

--- config/settings/production.py
+++ config/settings/production.py
@@ -65,7 +65,7 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['welikeminimal.com', ])
+ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['welikeminimal.com', ]).split(',')
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ['gunicorn', ]
